d6fcbdcc06844ac73b5b73aa626f8b459b6f7f1b
wombatstar <toxaf13@gmail.com>
2022-07-24 18:11:01 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,11 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
 const galleryItem = galleryItems.map((galleryItem) => 
 `<div class="gallery__item">
    <a class="gallery__link" href="${galleryItem.original}">
-      <img
-         class="gallery__image"
+      <img class="gallery__image"
          src="${galleryItem.preview}"
          data-source= "${galleryItem.original}" 
          alt="${galleryItem.description}"
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryElements = galleryItems.map(item => 
+    `<a class="gallery__item" href="${item.original}" >
+         <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            title="${item.description}"
+            />
+      </a>`
+      )
+      .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", galleryElements);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
